b295aa5d8abf6322a2f4b4dd3062b537a2ff03ca
efrem <efremzyohannes@gmail.com>
2022-10-02 22:29:45 -0500

    BalanceChecker setter getter methods added

--- src/main/java/testingpackage/BalanceChecker.java
+++ src/main/java/testingpackage/BalanceChecker.java
@@ -4,5 +4,19 @@ public class BalanceChecker {
     private AmountCalculator amount;
     private int balance;
 
+    public AmountCalculator getAmount() {
+        return amount;
+    }
 
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setAmount(AmountCalculator amount) {
+        this.amount = amount;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
