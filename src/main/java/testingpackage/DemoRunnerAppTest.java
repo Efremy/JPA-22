d45fa5f4fdc76a2210b941a61fb550933c851890
efrem <efremzyohannes@gmail.com>
2022-10-03 23:18:18 -0500

    modified DemoApp

--- src/main/java/testingpackage/DemoRunnerAppTest.java
+++ src/main/java/testingpackage/DemoRunnerAppTest.java
@@ -1,7 +1,14 @@
 package testingpackage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DemoRunnerAppTest {
+    @Autowired
+    private BalanceChecker balanceChecker;
+    @Autowired
+    private AmountCalculator amountCalculator;
     public static void main(String[] anrgs){
+        //BalanceChecker balanceChecker = new BalanceChecker();
         System.out.println("Good testing");
     }
 }
