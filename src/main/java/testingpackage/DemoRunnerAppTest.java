d6190fb332667bd6f670ed81fdf34b6fac76c565
efrem <efremzyohannes@gmail.com>
2022-10-04 22:40:33 -0500

    DemoApp comment added

--- src/main/java/testingpackage/DemoRunnerAppTest.java
+++ src/main/java/testingpackage/DemoRunnerAppTest.java
@@ -2,6 +2,7 @@ package testingpackage;
 
 public class DemoRunnerAppTest {
     public static void main(String[] anrgs){
+        //printing test
         System.out.println("Good testing");
     }
 }
