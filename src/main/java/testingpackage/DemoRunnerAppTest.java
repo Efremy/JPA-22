84cedad5a90b3ac0bd14d0e32a6a67e779fcc7d6
efrem <efremzyohannes@gmail.com>
2022-10-04 22:52:39 -0500

    added more comments to DemoApp

--- src/main/java/testingpackage/DemoRunnerAppTest.java
+++ src/main/java/testingpackage/DemoRunnerAppTest.java
@@ -4,5 +4,6 @@ public class DemoRunnerAppTest {
     public static void main(String[] anrgs){
         //printing test
         System.out.println("Good testing");
+        //need to add some new features here
     }
 }
