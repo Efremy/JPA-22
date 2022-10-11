16fa9499d2591ba225f9fbccec7b1a3f5b52be32
efrem <efremzyohannes@gmail.com>
2022-10-10 21:14:48 -0500

    uncommented DemoApp

--- src/main/java/testingpackage/DemoRunnerAppTest.java
+++ src/main/java/testingpackage/DemoRunnerAppTest.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class DemoRunnerAppTest {
 
-//    @Autowired
-//    private BalanceChecker balanceChecker;
-//    @Autowired
-//    private AmountCalculator amountCalculator;
-//    public static void main(String[] anrgs){
-//
-//        System.out.println("Good testing");
-//    }
+    @Autowired
+    private BalanceChecker balanceChecker;
+    @Autowired
+    private AmountCalculator amountCalculator;
+    public static void main(String[] anrgs){
+
+        System.out.println("Good testing");
+    }
 }
