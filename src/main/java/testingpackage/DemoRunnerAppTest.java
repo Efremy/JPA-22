f22c8a3a61bc83b808e683206420fc1e449fd87c
Efrem <68254678+Efremy@users.noreply.github.com>
2022-10-24 23:38:21 -0500

    Update DemoRunnerAppTest.java

--- src/main/java/testingpackage/DemoRunnerAppTest.java
+++ src/main/java/testingpackage/DemoRunnerAppTest.java
@@ -5,16 +5,16 @@ public class DemoRunnerAppTest {
         //printing test
         System.out.println("Good testing");
         //need to add some new features here
-    }
+   // }
 
 
 //    @Autowired
 //    private BalanceChecker balanceChecker;
 //    @Autowired
 //    private AmountCalculator amountCalculator;
-//    public static void main(String[] anrgs){
+    public static void main1(String[] anrgs){
 //
-//        System.out.println("Good testing");
-//    }
+        System.out.println("Good testing");
+    }
 
 }
